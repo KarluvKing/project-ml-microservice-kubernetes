8b8a998f2a1420cfeef99b8d5d309ff93df36e58
root <email@ruipaivabranco.com>
2019-08-22 01:11:35 +0200

    created script to upload docker container

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ruipbranco/app
 
 # Step 2:  
 # Authenticate & tag
+cat password_docker.txt | docker login --username=ruipbranco --password-stdin
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
