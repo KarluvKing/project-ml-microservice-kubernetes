8918d0ff55042b3b8f1a196bcfc56f7301a97d7b
root <email@ruipaivabranco.com>
2019-08-22 11:20:34 +0200

    created script run_kubernetes and outputfile for run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ruipbranco/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run kubernetes-nddevopskubernetes --image=$dockerpath --port=80 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
+
+echo "Waiting for POD to be ready..."
+sleep 560
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $POD_NAME 80:80
