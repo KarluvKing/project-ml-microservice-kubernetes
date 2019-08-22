550333a6214877108b064afcdb3a7e2d218620b2
root <email@ruipaivabranco.com>
2019-08-22 15:37:24 +0200

    log as info the output prediction of the input data

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"Output prediction: {prediction}")
     logging.basicConfig(filename='output_txt_files/docker_out.txt', level=logging.DEBUG)
     return jsonify({'prediction': prediction})
 
